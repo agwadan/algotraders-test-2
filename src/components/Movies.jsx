a7a5d36ea0872aa750801d67f54dadea80902dcb
agwadan <agwa.dan@gmail.com>
2022-02-15 13:03:53 +0300

    Fetch Movie Names Data

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { FetchMovieNames } from '../api/movie';
 
 const Movies = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    await FetchMovieNames().then((data) => setData(data))
+  }, [])
+
+  console.log(data);
   return (
-    <div>Movies</div>
+    <div>Movies
+
+      <select>
+        {
+          data.map((title, index) =>
+            <option key={index}>{title}</option>
+          )
+        }
+      </select>
+      <form action="">
+        <input>
+        </input>
+      </form>
+    </div>
   )
 }
 
